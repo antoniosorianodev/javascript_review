4ddbcd98cfb07e64063dc44659800bc565cc0eae
Antonio <asoriano@appdev.yearup.org>
2024-05-07 14:22:22 -0500

    completed numbers.js

--- scripts/numbers.js
+++ scripts/numbers.js
@@ -1,1 +1,23 @@
 "use strict"
+
+// 1.) Create a variable using the let keyword and assign a number to it.
+let paycheck = 100;
+
+// 2.) On a new line, add 15 to your variable
+paycheck += 15;
+
+// 3.) On a new line, subtract 8 from your variable without using the variable when calculating the new value
+paycheck -= 8;
+
+// 4.) Calculate the average of 26, 44, 72, 86, 112. Assign the calculated average to a new variable.
+let average = (26 + 44 + 72 + 86 + 112) / 5;
+
+// 5.) Modify the following code so it outputs 15 without adding any new lines of code.
+// let theNumber = 14;
+// console.log(theNumber++)
+
+let theNumber = 14;
+console.log(++theNumber);
+
+// 6.) What operator can you use to find out if a variable contains a number?
+// "typeof X" where X is the name of a variable will return "number", if the value of the given variable is a number
